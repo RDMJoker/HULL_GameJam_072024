6919ba1fdb029e746a655b24efc134d793ad441f
RDMJoker <45975936+RDMJoker@users.noreply.github.com>
2024-07-20 10:56:52 +0200

    push

---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DefaultNamespace
+{
+    public class ClickManager : MonoBehaviour
+    {
+        Vector3 currentMousePositionWorld;
+
+        public static Action<Vector3> OnLeftClick;
+        public static Action<Vector3> OnRightClick;
+        public void Point(InputAction.CallbackContext _context)
+        {
+            var value = _context.ReadValue<Vector2>();
+            if (Camera.main != null)
+            {
+                currentMousePositionWorld = Camera.main.ScreenToWorldPoint(value);
+                currentMousePositionWorld.z = 0f;
+            }
+
+            Debug.Log(currentMousePositionWorld);
+        }
+
+        public void LeftClick(InputAction.CallbackContext _context)
+        {
+            OnLeftClick.Invoke(currentMousePositionWorld);
+        }
+
+        public void RightClick(InputAction.CallbackContext _context)
+        {
+            OnRightClick.Invoke(currentMousePositionWorld);
+        }
+    }
+}
--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -8,11 +8,18 @@ namespace DefaultNamespace
     public class GridManager : MonoBehaviour
     {
         [SerializeField] Tilemap tilemap;
+        [SerializeField] int gridWidth;
+        [SerializeField] int gridHeight;
         ObjectGrid<ETileState> objectGrid;
+
+        void OnEnable()
+        {
+            ClickManager.OnLeftClick += 
+        }
 
         void Awake()
         {
-            objectGrid = new ObjectGrid<ETileState>(5, 5, 1, Vector3.zero);
+            objectGrid = new ObjectGrid<ETileState>(gridWidth, gridHeight, 1, Vector3.zero);
             for (int y = 0; y < objectGrid.Height; y++)
             {
                 for (int x = 0; x < objectGrid.Width; x++)
@@ -22,17 +29,28 @@ namespace DefaultNamespace
                    if (tilemap.GetTile(vectorIntPos) == null) continue;
                    if (tilemap.GetTile(vectorIntPos).name == "whitePixel")
                    {
-                       objectGrid.SetValue(vectorIntPos, ETileState.Occupied);
+                       objectGrid.SetValue(vectorIntPos, ETileState.Path);
+                   }
+                   else if (tilemap.GetTile(vectorIntPos).name == "blackPixel")
+                   {
+                       objectGrid.SetValue(vectorIntPos,ETileState.Free);
                    }
                 }
             }
+
+            int debugPathCount = 0;
+            int debugFreeCount = 0;
             for (int y = 0; y < objectGrid.Height; y++)
             {
                 for (int x = 0; x < objectGrid.Width; x++)
                 {
                     Debug.Log("X: " + x + "| Y: " + y + "| Type: " + objectGrid.GetValue(x,y));
+                    if (objectGrid.GetValue(x, y) == ETileState.Path) debugPathCount++;
+                    if (objectGrid.GetValue(x,y) == ETileState.Free) debugFreeCount++;
                 }
             }
+            Debug.Log("Paths: " + debugPathCount);
+            Debug.Log("Free: " + debugFreeCount);
         }
     }
 }
