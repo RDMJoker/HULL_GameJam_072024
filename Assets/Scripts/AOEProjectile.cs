112cd6c21990aafef2d5124a05c4dc8e7437b688
RDMJoker <45975936+RDMJoker@users.noreply.github.com>
2024-07-21 11:12:38 +0200

    assetpack and aoe bullets

---
+++ Assets/Scripts/AOEProjectile.cs
@@ -0,0 +1,49 @@
+﻿using DefaultNamespace.Enemies;
+using LL_Unity_Utils.Timers;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class AOEProjectile : Projectile
+    {
+
+        [SerializeField] ParticleSystem explosionEffect;
+
+        void Awake()
+        {
+            timer = new Timer(5);
+            timer.StartTimer();
+        }
+
+        void FixedUpdate()
+        {
+            if (timer.CheckTimer()) Destroy(gameObject);
+        }
+
+        public void SetDestination(Vector3 _targetPosition)
+        {
+            targetPosition = _targetPosition;
+        }
+
+
+        protected override void OnTriggerEnter2D(Collider2D _other)
+        {
+            if ((targetPosition - transform.position).magnitude < 0.6)
+            {
+                DoDamage();
+            }
+        }
+
+        protected override void DoDamage()
+        {
+            Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            var enemysInRange = Physics2D.OverlapCircleAll(transform.position, 1.5f, enemy);
+            foreach (var enemyCollider in enemysInRange)
+            {
+                enemyCollider.gameObject.GetComponent<Enemy>().TakeDamage(damage);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -9,11 +9,12 @@ namespace DefaultNamespace
     [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
     public class Projectile : MonoBehaviour
     {
-        float damage;
-        BoxCollider2D myCollider;
-        LayerMask enemy;
-        Enemy target;
-        Timer timer;
+       protected float damage;
+       [SerializeField] protected LayerMask enemy;
+       Enemy target;
+       protected Vector3 targetPosition;
+       protected Timer timer;
+
         void Awake()
         {
             timer = new Timer(5);
@@ -22,7 +23,7 @@ namespace DefaultNamespace
 
         void FixedUpdate()
         {
-            if(timer.CheckTimer()) Destroy(gameObject);
+            if (timer.CheckTimer()) Destroy(gameObject);
         }
 
         public void SetDamage(float _value)
@@ -30,20 +31,20 @@ namespace DefaultNamespace
             damage = _value;
         }
 
-        public void SetDestination( Enemy _target)
+        public void SetDestination(Enemy _target)
         {
             target = _target;
         }
 
 
-        void OnTriggerEnter2D(Collider2D _other)
+        protected virtual void OnTriggerEnter2D(Collider2D _other)
         {
-            if( target == null ||target.gameObject == null) return;
-            if (_other == null || target == null ||_other.gameObject != target.gameObject) return;
+            if (target == null || target.gameObject == null) return;
+            if (_other == null || target == null || _other.gameObject != target.gameObject) return;
             DoDamage();
         }
 
-        void DoDamage()
+        protected virtual void DoDamage()
         {
             target.TakeDamage(damage);
             Destroy(gameObject);
